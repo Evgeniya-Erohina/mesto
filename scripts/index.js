6e76c9da8742b249e2b5b9bb89e784491f45c550
Evgeniya Erohina <Eva200382@mail.ru>
2023-04-26 20:40:16 +0500

    исправления после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
 const openPopupBtn = document.querySelector('.profile__form-redaction');
 const popup = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.popup__button-close');
-const savePopupBtn = document.querySelector('.popup__button-save');
 const nameProfile = document.querySelector('.profile__form-name');
 const jobProfile = document.querySelector('.profile__form-job');
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
+let formElement = document.querySelector('.popup__container');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 openPopupBtn.addEventListener('click', openPopup);
 
@@ -18,37 +21,14 @@ function closePopup(evt) {
     popup.classList.remove('popup_opened');
   }
 }
-openPopupBtn.addEventListener('click', openPopup);
 
 popup.addEventListener('click', closePopup);
 
- // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
-
-// Находим поля формы в DOM
-let nameInput = document.getElementById('name');
-let jobInput = document.getElementById('job');
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit(evt) {
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
   evt.preventDefault();
+  popup.classList.remove('popup_opened');
 }
-  // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
 
- // Получите значение полей jobInput и nameInput из свойства value
- nameInput.value = nameProfile.textContent;
- jobInput.value = jobProfile.textContent;
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // Вставьте новые значения с помощью textContent
-
-  savePopupBtn.addEventListener('click', () => {
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
-  })
-
-  formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
